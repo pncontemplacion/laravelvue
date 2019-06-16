5eca7de58dbdeae8221288c75acd1d8a90338346
paulbcon2426 <paulbcon@gmail.com>
2019-06-16 14:39:44 -0400

    Update axios

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -146,7 +146,8 @@ class UserController extends Controller
         if ($search = \Request::get('q')) {
             $users = User::where(function($query) use ($search){
                 $query->where('name','LIKE',"%$search%")
-                        ->orWhere('email','LIKE',"%$search%");
+                        ->orWhere('email','LIKE',"%$search%")
+                        ->orWhere('type','LIKE',"%$search%");
             })->paginate(20);
         }else{
             $users = User::latest()->paginate(5);
